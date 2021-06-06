ee545ecff5ac9093656a3ef81459b4ca1e00ddc1
Bruno Paulino <brunojppb@gmail.com>
2021-06-06 23:07:14 +0200

    Add persistent layer via database process

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start(list_name) do
+    GenServer.start(__MODULE__, list_name)
   end
 
   def add_entry(todo_server, new_entry) do
@@ -15,22 +15,25 @@ defmodule Todo.Server do
   end
 
   @impl GenServer
-  def init(_) do
-    {:ok, Todo.List.new()}
+  def init(list_name) do
+    # Try to fetch it from disk. Fallback to empty list
+    initial_list = Todo.Database.get(list_name) || Todo.List.new()
+    {:ok, {list_name, initial_list}}
   end
 
   @impl GenServer
-  def handle_cast({:add_entry, new_entry}, todo_list) do
-    new_state = Todo.List.add_entry(todo_list, new_entry)
-    {:noreply, new_state}
+  def handle_cast({:add_entry, new_entry}, {list_name, todo_list}) do
+    new_todolist = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(list_name, new_todolist)
+    {:noreply, {list_name, new_todolist}}
   end
 
   @impl GenServer
-  def handle_call({:entries, date}, _, todo_list) do
+  def handle_call({:entries, date}, _, {list_name, todo_list}) do
     {
       :reply,
       Todo.List.entries(todo_list, date),
-      todo_list
+      {list_name, todo_list}
     }
   end
 end
